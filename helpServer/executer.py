06f8a4b74c212daec0ccbeb4f72c507f1faf34bb
SongYeongJu <thddudwn1012@naver.com>
2017-11-21 23:43:46 +0900

    파이선 서버와 통신하는 안드로이드 기초 추가

---
+++ helpServer/executer.py
@@ -0,0 +1,7 @@
+class Executer:
+    def __init__(self, tcpServer):
+        self.andRaspTCP = tcpServer
+ 
+    def startCommand(self, command):
+        self.andRaspTCP.sendAll(command)
+        
---
+++ helpServer/main.py
@@ -0,0 +1,29 @@
+import tcpServer
+import executer
+import queue
+import time
+ 
+# make public queue
+commandQueue = queue.Queue()
+ 
+# init module
+andRaspTCP = tcpServer.TCPServer(commandQueue, "", 7777)
+andRaspTCP.start()
+ 
+ 
+# set module to executer
+commandExecuter = executer.Executer(andRaspTCP)
+ 
+ 
+while True:
+    try:
+        command = commandQueue.get()
+        commandExecuter.startCommand(command)
+    except:
+        pass
+ 
+ 
+#while True:
+#    time.sleep(3)
+#    andRaspTCP.sendAll("321\n")
+    
